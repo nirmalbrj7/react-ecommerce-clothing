36f29576080d2874dcdd56dded354f0a4abdc37b
Nirmal Adhikari <nirmalbrj7@gmail.com>
2020-05-07 21:51:34 +0545

    add navigate route function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+    <div>
+        <h1> Hats Page</h1>
+    </div>
+)
+
 function App() {
   return (
     <div>
-       <HomePage/>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route exact path='/hats' component={HatsPage} />
+            </Switch>
+
+        </Router>
+
     </div>
   );
 }
