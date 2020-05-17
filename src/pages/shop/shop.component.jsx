5dee166d29e32aa0cffe124acd39d6895778b2c0
Nirmal Adhikari <nirmalbrj7@gmail.com>
2020-05-17 14:41:22 +0545

    remove logger in productions

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -11,7 +11,7 @@ import CollectionPage from "../collection/collection.component";
 
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
